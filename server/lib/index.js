78a07cc3ef04277ebcea4b1aae4a1b12330f6918
Pedro Eusebio <pedrosantos95@gmail.com>
2021-08-09 16:39:52 -0300

    adding docker-compose.yml

--- server/lib/index.js
+++ server/lib/index.js
@@ -2,13 +2,12 @@ const { createServer } = require("http");
 
 const createApplication = require("./app");
 const { port } = require("./config");
-const InMemoryUserRepository = require("./user/user.repository");
 
 const httpServer = createServer();
 
 createApplication(httpServer, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: "*",
   },
 });
 
